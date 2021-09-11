81431f00eaa63c352530dccb0889b086497b1f4b
Mpendulo Sibiya <mpendulodev@gmail.com>
2021-09-11 11:28:19 +0200

    adding google analytics :rocket:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  
+
+  constructor(public router: Router) {
+    let ga: Function;
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+  }
 }
